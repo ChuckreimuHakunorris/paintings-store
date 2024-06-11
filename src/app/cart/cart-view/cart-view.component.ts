596c66c2d077db70da83c5c5229926e0a7f2b49c
Ville Laaksonen <ville.laaksonen@wideflow.ai>
2024-06-11 13:10:23 +0300

    created cart component templates, service connection to api

---
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-cart-view',
+  templateUrl: './cart-view.component.html',
+  styleUrl: './cart-view.component.css',
+})
+export class CartViewComponent {}
